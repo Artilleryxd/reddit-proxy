200e3eb6e3bed2cc0f4c93059fd8d3152ebd9517
Artilleryxd <karandwari@gmail.com>
2025-05-15 12:24:44 +0530

    Created proxy server and routes

--- src/apis/proxy.js
+++ src/apis/proxy.js
@@ -1,14 +1,25 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors"
+import dotenv from "dotenv"
+import path from "path"
+import { fileURLToPath } from "url";
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 
-app.get("/home" , (req,res)=>{
-    res.json({
-        "msg" : "hello"
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+const accessToken = process.env.REDDIT_ACCESS_TOKEN
+
+
+app.get("/subscriptions" , async (req,res)=>{
+    axios.get("https://oauth.reddit.com/subreddits/mine/subscriber", {
+        headers:{
+            Authorization: `Bearer ${accessToken}`
+        }
     })
 })
 
